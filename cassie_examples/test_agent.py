05b2ec3ed7623b107a8d7408196ebe2cfcfd9ef1
Geoffrey Clark <clark.m.geoffrey@gmail.com>
2021-12-02 15:51:52 -0700

    updated and tested cassie_agent with functions

--- cassie_examples/test_agent.py
+++ cassie_examples/test_agent.py
@@ -185,8 +185,7 @@ rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
 
 py_driver.PyDriver(
     env,
-    py_tf_eager_policy.PyTFEagerPolicy(
-      random_policy, use_tf_function=True),
+    py_tf_eager_policy.PyTFEagerPolicy(random_policy, use_tf_function=True),
     [rb_observer],
     max_steps=initial_collect_steps).run(train_py_env.reset())
 
